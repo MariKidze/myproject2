b9077a60a728d41d790a34a69e17d208d62ed578
MariKidze <vashakidzemariy@gmail.com>
2023-03-10 15:39:14 +0400

    src/Main.java added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int ticketAmount = 10000;
+        int milesPerRub = 20;
+
+        int bonusMiles = ticketAmount/milesPerRub;
+
+        System.out.println("Рассчитать количество бонусных миль" + bonusMiles );
     }
 }
